299892b770f936e4e657f4e5fdf8d5e473db20a2
toni <ferreirocouto@gmail.com>
2020-01-13 21:35:01 +0100

    create serviceMap

---
+++ pet-clinic-data/src/main/java/com/toni/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.toni.petclinic.services.map;
+
+import com.toni.petclinic.model.Owner;
+import com.toni.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
