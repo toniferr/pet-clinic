c7dd930fc506cfa92cd18036b711295353a6ede5
toni <ferreirocouto@gmail.com>
2020-01-14 18:52:18 +0100

    add list owners and vets in view. update controllers

--- pet-clinic-data/src/main/java/com/toni/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/toni/petclinic/services/map/OwnerServiceMap.java
@@ -2,9 +2,11 @@ package com.toni.petclinic.services.map;
 
 import com.toni.petclinic.model.Owner;
 import com.toni.petclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
--- pet-clinic-web/src/main/java/com/toni/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/toni/petclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.toni.petclinic.model.Owner;
 import com.toni.petclinic.model.Vet;
 import com.toni.petclinic.services.OwnerService;
 import com.toni.petclinic.services.VetService;
-import com.toni.petclinic.services.map.OwnerServiceMap;
-import com.toni.petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +13,10 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    //@Autowire no hace falta(ya es como si lo tuviera)
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
