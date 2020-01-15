12110e7a7c53171a454fd699369eda2815124f5e
toni <ferreirocouto@gmail.com>
2020-01-15 19:42:52 +0100

    edit model/service map

---
+++ pet-clinic-data/src/main/java/com/toni/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.toni.petclinic.services;
+
+import com.toni.petclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/toni/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,34 @@
+package com.toni.petclinic.services.map;
+
+import com.toni.petclinic.model.Speciality;
+import com.toni.petclinic.services.SpecialtiesService;
+
+import java.util.Set;
+
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
