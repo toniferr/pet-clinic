e41cc2f70767c3f433146b82e9172bf0ff2060b2
toni <ferreirocouto@gmail.com>
2020-01-26 18:14:09 +0100

    add list vets/ api rest vets

--- pet-clinic-web/src/main/java/com/toni/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/toni/petclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package com.toni.petclinic.controllers;
 
+import com.toni.petclinic.model.Vet;
 import com.toni.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -14,11 +18,16 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html","/vets.html"})
+    @GetMapping({"/vets","/vets/index","/vets/index.html","/vets.html"})
     public String getVets(Model model){
 
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 
 }
